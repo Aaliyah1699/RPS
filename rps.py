0cc81eaa7118920f7e1c9cacbae698fa81a99ed1
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-02 21:22:23 +0000

    Completed comments for 1st func

--- rps.py
+++ rps.py
@@ -3,4 +3,18 @@ import random
 import os
 import re
 
-# function to check if the user wants to play
+#function to check if the user wants to play
+
+#while the response is True
+
+#Try a response to see if they want to play again
+
+#if they say something other than yes/ no raise value error with their options
+
+#if their answer is yes return True
+
+#else use os to clear and exit game
+
+#Except value error as err and print err
+ 
+ 
